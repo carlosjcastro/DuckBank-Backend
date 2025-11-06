b594cf7a54d310b7532419d74e4b795add44d691
Castro_Galante_Carlos_José <carlosjosecastro.p@gmail.com>
2025-11-06 18:25:10 -0300

    modificando ruta principal en urls.py

--- duckbank/urls.py
+++ duckbank/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("¡Bienvenido a DuckBank Backend!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('loans.urls')),
+    path('', home),
 ]
